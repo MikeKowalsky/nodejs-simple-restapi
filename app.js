3434c24a365886544b3b9c6086bba5e3602dbc4f
MikeKowalsky <mkowalewski83@gmail.com>
2019-03-16 18:42:26 +0100

    small improvements

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = express();
 
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://s.codepen.io");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
